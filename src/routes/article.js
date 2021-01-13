37306315b50f3ec4165c7150e30ad2085be6fb53
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-13 17:02:05 +0100

    Let's go to the debrief

--- src/routes/article.js
+++ src/routes/article.js
@@ -11,8 +11,8 @@ const route = express.Router();
     GET /articles/:id/reviews => returns all the reviews for the specified article OK
     GET /articles/:id/reviews/:reviewId => returns a single review for the specified article OK
     POST /articles/:id => adds a new review for the specified article OK
-    PUT /articles/:id/reviews/:reviewId => edit the review belonging to the specified article
-    DELETE /articles/:id/reviews/:reviewId => delete the review belonging to the specified article
+    PUT /articles/:id/reviews/:reviewId => edit the review belonging to the specified article OK
+    DELETE /articles/:id/reviews/:reviewId => delete the review belonging to the specified article OK
 
 */
 
