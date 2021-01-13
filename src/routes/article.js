369357b7b87b9cc9256b305a2947d6a611ffb21f
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-13 16:59:35 +0100

    ALL DONE

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const Article = require("./schema");
 const route = express.Router();
 
 /*
+    Your backend should now have the possibility to add a review to an
+    article. Mongo's preferred data design should be to embed reviews into
+    articles, therefore you should implement the following endpoints
 
- GET /articles => returns the list of articles
-    GET /articles/:id => returns a single article
-    POST /articles => create a new article
-    PUT /articles/:id => edit the article with the given id
-    DELETE /articles/:id => delete the article with the given id
+    GET /articles/:id/reviews => returns all the reviews for the specified article OK
+    GET /articles/:id/reviews/:reviewId => returns a single review for the specified article OK
+    POST /articles/:id => adds a new review for the specified article OK
+    PUT /articles/:id/reviews/:reviewId => edit the review belonging to the specified article
+    DELETE /articles/:id/reviews/:reviewId => delete the review belonging to the specified article
 
 */
 
@@ -68,4 +72,135 @@ route.delete("/:id", async (req, res, next) => {
   }
 });
 
+/*<------------------------------------------------- ----------------------------------- --------------------------------------> */
+
+route.get("/:id/reviews", async (req, res, next) => {
+  try {
+    // Accedere articolo e poi prendere reviews
+    const { reviews } = await Article.findById(req.params.id);
+
+    res.status(201).send(reviews);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+route.get("/:id/reviews/:id2", async (req, res, next) => {
+  const reviewId = req.params.id2;
+  try {
+    // Accedere articolo e poi prendere reviews
+    const { reviews } = await Article.findById(req.params.id, {
+      _id: 0,
+      reviews: {
+        $elemMatch: { _id: mongoose.Types.ObjectId(req.params.id2) },
+      },
+    });
+
+    if (reviews) {
+      res.status(201).send(reviews);
+    } else {
+      console.log("error");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+route.delete("/:id/reviews/:id2", async (req, res, next) => {
+  try {
+    const reviewToDelete = await Article.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          reviews: { _id: mongoose.Types.ObjectId(req.params.id2) },
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.send(reviewToDelete);
+  } catch (error) {
+    console.log(error);
+  }
+});
+route.put("/:id/reviews/:id2", async (req, res, next) => {
+  const reviewId = req.params.id2;
+  try {
+    // Accedere articolo e poi prendere reviews
+    const { reviews } = await Article.findById(req.params.id, {
+      _id: 0,
+      reviews: {
+        $elemMatch: { _id: mongoose.Types.ObjectId(req.params.id2) },
+      },
+    });
+
+    if (reviews) {
+      const updatedReview = { ...reviews[0].toObject(), ...req.body };
+
+      modifiedReview = await Article.findByIdAndUpdate(
+        {
+          _id: mongoose.Types.ObjectId(req.params.id1),
+          "reviews._id": mongoose.Types.ObjectId(req.params.id2),
+        },
+        { $set: { "reviews.$": updatedReview } },
+        {
+          runValidators: true,
+          new: true,
+        }
+      );
+      res.status(201).send(updatedReview);
+    } else {
+      console.log("error");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+route.post("/:id/reviews", async (req, res, next) => {
+  try {
+    // Prendo l'articolo by ID e push all'interno dell array corrispondente a reviews il mio req. body
+    updateArticles = await Article.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          reviews: req.body,
+        },
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+
+    res.status(201).send(updateArticles);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = route;
+
+/*
+
+try {
+    const modifiedBook = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          purchaseHistory: { _id: mongoose.Types.ObjectId(req.params.bookId) },
+        },
+      },
+      {
+        new: true,
+      }
+    )
+    res.send(modifiedBook)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+
+*/
--- src/routes/schema.js
+++ src/routes/schema.js
@@ -14,6 +14,12 @@ const ArticleSchema = new Schema(
       name: String,
       img: String,
     },
+    reviews: [
+      {
+        text: String,
+        user: String,
+      },
+    ],
     cover: String,
   },
   {
@@ -22,3 +28,23 @@ const ArticleSchema = new Schema(
 );
 
 module.exports = model("Article", ArticleSchema);
+
+/**
+ * {
+    "headLine": "WELCOME",
+  "subHead": "Just subtitle",
+  "content": "YOU THINK YOU HOW TO DANCE",
+  "category": "STUPID",
+  "author": {
+    "name": "DevRichard",
+    "img": "https://source.unsplash.com/random"
+  }, 
+  "reviews": [
+      {
+        "text": "WONDERFUL ARTICLE LIKE IT.",
+        "user": "Richard"
+      }
+    ],
+  "cover": "random stufffffs"
+}
+ */
